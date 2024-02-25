2d88583c1fe88155aa78cde7ccd06f60c411a8dd
Ayu <ayud2898@gmail.com>
2024-02-25 13:55:56 +0800

    update negative login & add product to cart

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -6,12 +6,23 @@ class LoginPage extends Page {
     get fieldUsername () { return $('#user-name');}
     get fieldPassword () { return $('#password');}
     get buttonLogin () { return $('#login-button');}
+    get lockedoutErrorLogin () { return $('//h3[text()="Epic sadface: Sorry, this user has been locked out."]');}
+    get errorLoginWithoutCredential () { return $('//h3[text()="Epic sadface: Username is required"]');}
 
     async login (username, password) {
         await this.fieldUsername.setValue(username);
         await this.fieldPassword.setValue(password);
         await this.buttonLogin.click();
     }
+
+    async userLockedOutErrorLogin () {
+        await expect(this.lockedoutErrorLogin).toBeDisplayed();
+    }
+
+    async LoginWithoutCredential () {
+        await expect(this.errorLoginWithoutCredential).toBeDisplayed();
+    }
+
 
     open () {
         return super.open('/'); //NOTE: Will open https://www.saucedemo.com/
--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,58 +1,54 @@
 
 const LoginPage = require('../pageobjects/login.page')
 const HomePage = require('../pageobjects/home.page')
+const loginPage = require('../pageobjects/login.page')
 
 describe('Swag Lab', () => {
 
-    // Positive case
+    //TODO: Create Positive Case Login
     it('should login with standard_user credentials', async () => {
         await LoginPage.open()
         await LoginPage.login(process.env.USERNAME_STANDAR_USER, process.env.PASSWORD_SAUCEDEMO)
         await HomePage.validdateHomePage()  
-        //TODO: akan membuat positive case
     })
 
-    // Positive case
-    it('should add to cart with standard_user spesific product', async () => {
+    //TODO: Create Positive Case Add Product
+    it('should add spesific product to cart with standard_user', async () => {
         await LoginPage.open()
         await LoginPage.login(process.env.USERNAME_STANDAR_USER, process.env.PASSWORD_SAUCEDEMO)
         await HomePage.validdateHomePage()  
         const btnAddToCartElement = await $('#add-to-cart-sauce-labs-backpack')
         await btnAddToCartElement.click()
-        //TODO: akan membuat positive case
     })
 
-    // Positive case
+    //TODO: Create Positive Case Login
     it('should login with problem_user credentials', async () => {
         await LoginPage.open()
         await LoginPage.login(process.env.USERNAME_PROBLEM_USER, process.env.PASSWORD_SAUCEDEMO)
         await HomePage.validdateHomePage()  
-        //TODO: akan membuat positive case
     })
 
-    // Negativ case
+    //TODO: Create Positive Case Add Product
+    it('should add spesific product to cart with problem_user', async () => {
+        await LoginPage.open()
+        await LoginPage.login(process.env.USERNAME_PROBLEM_USER, process.env.PASSWORD_SAUCEDEMO)
+        await HomePage.validdateHomePage()  
+        const btnAddToCartElement = await $('#add-to-cart-sauce-labs-bolt-t-shirt')
+        await btnAddToCartElement.click()
+    })
+
+    //TODO: Create Negative Case Login
     it('should login with locked_out_user credentials', async () => {
         await LoginPage.open()
         await LoginPage.login(process.env.USERNAME_LOCKEDOUT_USER, process.env.PASSWORD_SAUCEDEMO)
-        const errorElement = await $('[data-test="error"]');
-        await expect(errorElement).toBeDisplayed();
-
-        const errorMessage = await errorElement.getText();
-        await expect(errorMessage).toEqual('Epic sadface: Sorry, this user has been locked out.');
-        //TODO: akan membuat negative case
+        await LoginPage.userLockedOutErrorLogin()
     })
 
-    // Negativ case
+    //TODO: Create Negative Case Login Without Username & Password
     it('should login without credentials', async () => {
         await LoginPage.open()
         await LoginPage.login("", "")
-
-        const errorElement = await $('[data-test="error"]');
-        await expect(errorElement).toBeDisplayed();
-
-        const errorMessage = await errorElement.getText();
-        await expect(errorMessage).toEqual('Epic sadface: Username is required');
-        //TODO: akan membuat negative case
+        await LoginPage.LoginWithoutCredential()
     })
 })
 
